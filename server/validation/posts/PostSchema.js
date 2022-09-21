f203879b922da86cac0a2f2024f2b47e2a773850
Saeed99Madi <saed.zooro@gmail.com>
2022-09-21 18:45:19 +0300

    add user posts route related to #19

--- server/validation/posts/PostSchema.js
+++ server/validation/posts/PostSchema.js
@@ -1,6 +1,6 @@
-const joi = require('joi')
+const joi = require('joi');
 
 module.exports = joi.object({
-    title: joi.string().alphanum().min(7).max(100).required(),
-    content: joi.string().alphanum().min(7).max(100).required(),
-})
+  title: joi.string().min(7).max(100).required(),
+  content: joi.string().min(7).max(100).required(),
+});
